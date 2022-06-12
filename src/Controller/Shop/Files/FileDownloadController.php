f510f654486921c0d2835bbca310e3f58175adb2
jsmarchewka <jsmarchewka@gmail.com>
2022-06-12 21:28:00 +0200

    files finish

---
+++ src/Controller/Shop/Files/FileDownloadController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller\Shop\Files;
+
+use App\Entity\AdminFile;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FileDownloadController extends AbstractController
+{
+
+    #[Route('/files', name: 'app_shop_files')]
+    public function __invoke(EntityManagerInterface $entityManager): Response
+    {
+        $files = $entityManager->getRepository(AdminFile::class)->findAll();
+        return $this->render('shop/files/index.html.twig', [
+            'files' => $files,
+        ]);
+    }
+}
